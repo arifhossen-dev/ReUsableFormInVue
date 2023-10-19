489e860e915f82a8d3c579bab03b71f82100b0ca
arifhossen-dev <ahak.bsl@gmail.com>
2023-10-19 16:22:03 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SimpleForm from './components/SimpleForm.vue';
+import TailForm from './components/TailForm.vue';
+import TailFormReUsable from './components/TailFormReUsable.vue';
+</script>
+<template>
+  <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="flex">
+      <TailForm />
+      
+      <TailFormReUsable />
+    </div>
+    <div
+      class="flex items-center justify-center py-3 my-10 font-bold text-red-500 border-4 border-dashed rounded-2xl border-green-400/40 bg-blue-300/30">
+      The divider
+    </div>
+    <SimpleForm />
+  </div>
+</template>
