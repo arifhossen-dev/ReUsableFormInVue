0625b947e03e022f027a8740f9313a1e67c3e491
arifhossen-dev <ahak.bsl@gmail.com>
2023-10-21 21:56:49 +0600

    BaseRadioGroup created

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -37,20 +37,10 @@
 
       <h3>Are pets allowed?</h3>
       <div>
-        <BaseRadio
+        <BaseRadioGroup
           v-model="event.pets"
-          value="1"
-          label="Yes"
           name="pets"
-        />
-      </div>
-
-      <div>
-        <BaseRadio
-          v-model="event.pets"
-          value="0"
-          label="No"
-          name="pets"
+          :options="petOptions"
         />
       </div>
 
@@ -78,6 +68,7 @@
 import BaseCheckbox from '../components/BaseCheckbox.vue';
 import BaseInput from '../components/BaseInput.vue';
 import BaseRadio from '../components/BaseRadio.vue';
+import BaseRadioGroup from '../components/BaseRadioGroup.vue';
 import BaseSelect from '../components/BaseSelect.vue';
 
 export default {
@@ -102,9 +93,13 @@ export default {
                     catering: false,
                     music: false
                 }
-            }
+            },
+            petOptions:[
+              {label:'Yes', value: 1},
+              {label:'No', value: 0},
+            ]
         };
     },
-    components: { BaseInput, BaseSelect, BaseCheckbox, BaseRadio }
+    components: { BaseInput, BaseSelect, BaseCheckbox, BaseRadio, BaseRadioGroup }
 }
 </script>
