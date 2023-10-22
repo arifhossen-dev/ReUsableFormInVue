187b515063327f5b47ce3180dc890b92f9253989
arifhossen-dev <ahak.bsl@gmail.com>
2023-10-22 16:35:32 +0600

    forms are ready

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import ComponentsForm from './views/ComponentsForm.vue';
+import LoginForm from './views/LoginForm.vue';
 import SimpleForm from './views/SimpleForm.vue';
 </script>
 <template>
   <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <LoginForm />
+    <hr>
     <SimpleForm />
+    <hr>
+    <ComponentsForm/>
   </div>
 </template>
